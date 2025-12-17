0c1d465b012e271aae2d4c88c5ef1b6d3ae6a82a
Andres Santiago Monsalve <santi.monsalve09@gmail.com>
2025-12-16 22:59:48 -0500

    ajuste appsetings

--- GestionBD.API/Program.cs
+++ GestionBD.API/Program.cs
@@ -1,6 +1,12 @@
+using GestionBD.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Registrar DbContext con la cadena de conexión desde appsettings.json
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- GestionBD.Infraestructure/Data/ApplicationDbContext.cs
+++ GestionBD.Infraestructure/Data/ApplicationDbContext.cs
@@ -33,8 +33,9 @@ public partial class ApplicationDbContext : DbContext
     public virtual DbSet<TblParametro> TblParametros { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.,8093;Database=gestionbd;User Id=sa;Password=Admin123456!;TrustServerCertificate=true;");
+    {
+        // La configuración se hace desde Program.cs usando Dependency Injection
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
