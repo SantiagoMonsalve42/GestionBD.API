39b60be2e4118bfbbc505bf40e47b3fadbafc453
Andrés Santiago Monsalve Salinas <Asmonsalves@intergrupo.com>
2026-01-04 12:05:27 -0500

    eliminar admin de archivos

--- GestionBD.Domain/Interfaces/IUploadDelivery.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GestionBD.Domain.Interfaces
-{
-    public interface IUploadDelivery
-    {
-        Task<bool> uploadFile(Stream zipFile, string fileName, CancellationToken cancellationToken);
-    }
-}
