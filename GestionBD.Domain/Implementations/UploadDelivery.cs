d4bda6a428cc6d6bac941733aa4dfeaba5826168
Andres Santiago Monsalve <santi.monsalve09@gmail.com>
2026-01-03 20:01:09 -0500

    funcionalidad para subir archivo generico

---
+++ GestionBD.Domain/Implementations/UploadDelivery.cs
@@ -0,0 +1,40 @@
+﻿using GestionBD.Domain.Interfaces;
+using System.Threading;
+
+namespace GestionBD.Domain.Implementations
+{
+    public class UploadDelivery : IUploadDelivery
+    {
+        private string route;
+        public UploadDelivery(string route) { 
+            this.route = route;
+        }
+
+        public async Task<bool> uploadFile(Stream zipFile, string fileName, CancellationToken cancellationToken)
+        {
+            if (zipFile == null)
+                throw new ArgumentNullException(nameof(zipFile));
+
+            if (string.IsNullOrWhiteSpace(this.route))
+                throw new ArgumentException("Ruta destino inválida", nameof(this.route));
+
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentException("Nombre de archivo inválido", nameof(fileName));
+
+            Directory.CreateDirectory(this.route);
+
+            var rutaCompleta = Path.Combine(this.route, fileName);
+
+            await using var fileStream = new FileStream(
+                rutaCompleta,
+                FileMode.Create,
+                FileAccess.Write,
+                FileShare.None,
+                bufferSize: 81920,
+                useAsync: true);
+
+            await zipFile.CopyToAsync(fileStream, cancellationToken);
+            return true;
+        }
+    }
+}
---
+++ GestionBD.Domain/Interfaces/IUploadDelivery.cs
@@ -0,0 +1,7 @@
+﻿namespace GestionBD.Domain.Interfaces
+{
+    public interface IUploadDelivery
+    {
+        Task<bool> uploadFile(Stream zipFile, string fileName, CancellationToken cancellationToken);
+    }
+}
