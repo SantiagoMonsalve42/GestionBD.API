128346e5ea4b2d1e38e8ae4c76cc276930c7f23e
Andrés Santiago Monsalve Salinas <Asmonsalves@intergrupo.com>
2026-01-05 16:46:00 -0500

    eliminar en cascada

--- GestionBD.Application/Entregables/Commands/DeleteEntregableCommandHandler.cs
+++ GestionBD.Application/Entregables/Commands/DeleteEntregableCommandHandler.cs
@@ -1,22 +1,26 @@
-using MediatR;
+using GestionBD.Application.Abstractions;
 using GestionBD.Domain;
 using GestionBD.Domain.Entities;
+using MediatR;
 
 namespace GestionBD.Application.Entregables.Commands;
 
 public sealed class DeleteEntregableCommandHandler : IRequestHandler<DeleteEntregableCommand, Unit>
 {
     private readonly IUnitOfWork _unitOfWork;
-
-    public DeleteEntregableCommandHandler(IUnitOfWork unitOfWork)
+    private readonly IFileStorageService _fileStorageService;
+    public DeleteEntregableCommandHandler(IUnitOfWork unitOfWork,IFileStorageService fileStorageService)
     {
         _unitOfWork = unitOfWork;
+        _fileStorageService = fileStorageService;
     }
 
     public async Task<Unit> Handle(DeleteEntregableCommand command, CancellationToken cancellationToken)
     {
         var entregable = await _unitOfWork.FindEntityAsync<TblEntregable>(command.IdEntregable, cancellationToken);
-
+        
+        await _fileStorageService.DeleteFileAsync(entregable.RutaEntregable);
+        
         if (entregable == null)
             throw new KeyNotFoundException($"Entregable con ID {command.IdEntregable} no encontrado.");
 
